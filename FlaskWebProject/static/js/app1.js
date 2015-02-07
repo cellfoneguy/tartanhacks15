ea8a7841ecdbe32090d129360babbfe7de09902b
cellfoneguy <cellfoneguy@gmail.com>
2015-02-07 15:04:08 -0500

    friends

--- FlaskWebProject/static/js/app1.js
+++ FlaskWebProject/static/js/app1.js
@@ -77,7 +77,6 @@
     FB.api('/me', function(response) {
       console.log('Successful login for: ' + response.name);
     document.getElementById('fbname').innerHTML = response.name;
-    document.getElementById('fbgender').innerHTML = response.gender;
       document.getElementById('status').innerHTML =
         'Thanks for logging in, ' + response.name + '!';
       return response;
