a28b8b24f7b1d95bc15b404b78839582ecb1b2df
dwkang1111 <dwkang1111@gmail.com>
2015-02-07 07:04:02 -0500

    Basic Structure

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -1,41 +1,9 @@
-#from __future__ import with_statement # for Python 2.5 and 2.6
 from flask import Flask, request, redirect, render_template, g
-#import os
 app = Flask(__name__)
-
-email_addresses = []
-
-def saveMap(contents): #actual saver function
-	path = "saved.txt"
-	#if os.path.exists(path):
-	#	os.remove(path)
-	savedMap = open(path,"wt")
-	savedMap.write(contents)
-	savedMap.close()
-
-def loadmap():
-	path = "saved.txt"
-	savedMap = open(path, "r")
-	contents = savedMap.read()
-	return contents
 
 @app.route('/')
 def hello_world():
 	return render_template('index.html')
-
-@app.route('/signup', methods = ['POST'])
-def signup():
-	email = request.form['email']
-	#email_addresses.append(email)
-	#print(email_addresses)
-	past = loadmap()
-	saveMap(past + '\n' + email)
-	return redirect('/')
-
-@app.route('/emails.html')
-def emails():
-	email_addresses = loadmap().split('\n')
-	return render_template('emails.html', email_addresses=email_addresses)
 
 if __name__ == "__main__":
     app.run(debug = True)
