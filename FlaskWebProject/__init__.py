aa7be73000873800f2ced8e5d5b11c0455660535
dwkang1111 <dwkang1111@gmail.com>
2015-02-07 05:07:43 -0500

    re

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -4,10 +4,6 @@ app = Flask(__name__)
 
 email_addresses = []
 
-@app.before_request
-def before_request():
-	g.db = sqlite3.connect("emails.db")
-
 @app.route('/')
 def hello_world():
 	return render_template('index.html')
@@ -15,19 +11,13 @@ def hello_world():
 @app.route('/signup', methods = ['POST'])
 def signup():
 	email = request.form['email']
-	g.db.execute("INSERT INTO email_addresses VALUES (?)", [email])
-	g.db.commit()
+	email_addresses.append(email)
+	print(email_addresses)
 	return redirect('/')
 
 @app.route('/emails.html')
 def emails():
-	email_addresses = g.db.execute("SELECT email FROM email_addresses").fetchall()
 	return render_template('emails.html', email_addresses=email_addresses)
-
-@app.teardown_request
-def teardown_request(exception):
-	if hasattr(g, 'db'):
-		g.db.close()
 
 if __name__ == "__main__":
     app.run(debug = True)
