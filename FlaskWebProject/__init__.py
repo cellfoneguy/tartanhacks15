500b64587ad44ca7c011c01c343999817f467c72
dwkang1111 <dwkang1111@gmail.com>
2015-02-07 10:54:37 -0500

    add alex

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -1,14 +1,11 @@
 from flask import Flask, request, redirect, render_template, g
-#import alex
+import alex
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return render_template('index.html')
 
-@app.route('/testing')
-def contact():
-	return render_template('contact.html')
+	return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug = True)
