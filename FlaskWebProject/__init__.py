5d62dc248f30dc1c64af6c6fcb56babbf3f29428
dwkang1111 <dwkang1111@gmail.com>
2015-02-07 04:28:56 -0500

    Email changes

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -1,11 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, request, redirect, render_template
 app = Flask(__name__)
+
+email_addresses = []
 
 @app.route('/')
 def hello_world():
-	author = "someone"
-	name = "User"
-	return render_template('index.html', author=author, name=name)
+	return render_template('index.html')
+
+@app.route('/signup', methods = ['POST'])
+def signup():
+	email = request.form['email']
+	email_addresses.append(email)
+	print(email_addresses)
+	return redirect('/')
+
+@app.route('/emails.html')
+def emails():
+	return render_template('emails.html', email_addresses=email_addresses)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
