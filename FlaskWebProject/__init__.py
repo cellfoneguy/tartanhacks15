0720c4454bac8bca07a454e32371cd2130dc7e2d
dwkang1111 <dwkang1111@gmail.com>
2015-02-07 01:36:55 -0500

    asdf

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -1,21 +1,13 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import render_template
+from app import app
 
 @app.route('/')
-def hello_world():
-	return 'Hello World!'
-
-@app.route('/hello')
-def hello():
-	return "hi"
-
-@app.route('/user/<username>')
-def show_user_profile(username):
-	return 'User %s' % username
-
-@app.route('/post/<int:post_id')
-def show_post(post_id):
-	return 'Post %d' % post_id
+@app.route('/index')
+def index():
+	user = {'nickname': 'Paul'}
+	return render_template('index.html',
+							title='Home',
+							user=user)
 
 if __name__ == '__main__':
 	app.run()
