0b07ed5cb8e83819d37f2985d986117a4d9b3776
cellfoneguy <cellfoneguy@gmail.com>
2015-02-07 12:30:38 -0500

    moved app.js

--- FlaskWebProject/static/js/app.js
+++ FlaskWebProject/static/js/app.js
@@ -72,11 +72,17 @@
 
   // Here we run a very simple test of the Graph API after login is
   // successful.  See statusChangeCallback() for when this call is made.
-  function testAPI() {
+  function getUserInfo() {
     console.log('Welcome!  Fetching your information.... ');
     FB.api('/me', function(response) {
       console.log('Successful login for: ' + response.name);
       document.getElementById('status').innerHTML =
         'Thanks for logging in, ' + response.name + '!';
+      return response;
     });
   }
+
+$('yolo').innerHTML = getUserInfo().name;
+
+
+  module.exports.testAPI=testAPI;
